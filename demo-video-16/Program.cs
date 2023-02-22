695dcc690d726c6df9cfea9b2d8f06e658c4de10
Henrique Eduardo Souza <h.eduardo.souza@avanade.com>
2023-02-22 00:12:17 -0300

    Inclusão Demo 16

---
+++ demo-video-16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+string nome = "Henrique";
+string sobrenome = "Souza";
+nome += " Eduardo";
+nome += " " + sobrenome;
+Console.WriteLine(nome);
+
+StringBuilder stringBuilder = new StringBuilder();
+stringBuilder.Append("Henrique");  
+stringBuilder.Append(" Eduardo");
+stringBuilder.Append(" ");
+stringBuilder.Append("Souza");
+console.WriteLine(stringBuilder.ToString());
